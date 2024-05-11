f1f418c18904b5b08b0bdef5ccd63faa6295026d
Besfort Ramadani <71851042+BesRam@users.noreply.github.com>
2024-05-11 18:54:11 +0200

    add tests for ToDo to improve sonar report test coverage

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
@@ -14,4 +14,21 @@ public class ToDoTest {
         assertEquals(1, toDo1.getId());
     }
     
+    @Test
+    public void testSetters() {
+        var toDo2 = new ToDo();
+        toDo2.setId(2);
+
+        assertEquals(2, toDo2.getId());
+        assertEquals(null, toDo2.getTitle());
+        assertEquals(null, toDo2.getDescription());
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        var toDo3 = new ToDo();
+        assertEquals(0, toDo3.getId());
+        assertEquals(null, toDo3.getTitle());
+        assertEquals(null, toDo3.getDescription());
+    }
 }
