c1b8ce8db119f4ec83db51ae9b6c61ea0b26b75a
Besfort Ramadani <71851042+BesRam@users.noreply.github.com>
2024-05-11 20:36:42 +0200

    implement correct code for failed test fizzbuzz

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
@@ -3,7 +3,9 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 public class FizzBuzzConverter {
 
     public String convert(int i) {
-        if (i % 3 == 0 && i % 7 == 0) {
+        if (i % 11 == 0) {
+            return "Boom";
+        } else if (i % 3 == 0 && i % 7 == 0) {
             return "FizzBuzz";
         } else if (i % 3 == 0) {
             return "Fizz";
@@ -11,6 +13,6 @@ public class FizzBuzzConverter {
             return "Buzz";
         }
         return String.valueOf(i);
-    }
+    }    
 
 }
