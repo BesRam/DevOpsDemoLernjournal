3f8263e62940b088c60db110f70fe087b23c9ad0
Besfort Ramadani <71851042+BesRam@users.noreply.github.com>
2024-05-11 16:23:53 +0200

    implement move making and player alternating logic in TicTacToe

--- backend/src/main/java/ch/zhaw/iwi/devops/tictactoe/TicTacToe.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/tictactoe/TicTacToe.java
@@ -2,15 +2,27 @@ package ch.zhaw.iwi.devops.tictactoe;
 
 public class TicTacToe {
     private char[][] board;
+    private char currentPlayer;
 
     public TicTacToe() {
         board = new char[3][3];
-        // Initialisiere das Board mit Leerzeichen
+        currentPlayer = 'X'; // X beginnt das Spiel
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = ' ';
             }
         }
+    }
+
+    public void makeMove(int row, int col) {
+        if (board[row][col] == ' ') { // Prüft, ob das Feld leer ist
+            board[row][col] = currentPlayer; // Setzt den Markierungswert des aktuellen Spielers
+            togglePlayer(); // Wechselt den Spieler
+        }
+    }
+
+    private void togglePlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
 
     public char getMarkAt(int row, int col) {
